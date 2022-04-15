01390c82a5e8dc50ec3b7a56028357eca91fb94a
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-15 12:43:50 +0530

    bcrypt using Mutators

--- app/Http/Controllers/Crud.php
+++ app/Http/Controllers/Crud.php
@@ -7,14 +7,27 @@ use App\Models\User;
 
 class Crud extends Controller
 {
+    //CRUD operations using Eloquent ORM
     //CREATE-----------------------------------------------------
     function registerData(Request $req)
     {
         $user = new User;
         $user->name = $req->name;   //key should be table field
-        $user->password = $req->password;
+        $user->password = $req->password;   //bcrypt used in model using Mutators
+        $user->email = $req->email;
         $user->save();
         return $req->input();
+
+        //OR
+
+        // $data = [
+        //     'name' => $req->name,
+        //     'password' => bcrypt($req->password);
+        //     'email' => $req->email
+        // ];
+        // User::create($data);
+        // return $req->input();
+
     }
 
     //READ--------------------------------------------------------
@@ -39,7 +52,7 @@ class Crud extends Controller
         $updateFromId->save();
         return redirect('myuser');
     }
-    
+
     //DELETE-----------------------------------------------------
     function deleteUser($id)
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,5 +10,15 @@ class User extends Model
     use HasFactory,HasApiTokens;
     public $table="register";   //define if DB name is not registers
     public $timestamps = false;
+    protected $guarded = []; //FIX for - SQLSTATE[HY000]: General error: 1364 Field 'email' doesn't have a default value
 
+
+    //Mutator & Accessors
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    public function getNameAttribute($name){
+        return ucfirst($name);
+    }
 }
