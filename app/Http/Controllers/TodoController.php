fffda7076c7120629718d8f430ce6fc4fbf96428
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-23 23:30:11 +0530

    todo controller for CRUD operation with route model binding and form request validation

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,34 +7,48 @@ use App\Models\Todo;
 use Faker\Provider\Lorem;
 use Nette\Utils\Validators;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\TodoCreateRequest;
 
 class TodoController extends Controller
 {
-    function index(){
-return view('todos.index');
+    function index()
+    {
+        $mytodo = Todo::all();
+        return view ('todos.index', ['mytodos' => $mytodo]);
     }
 
-    function create(){
-        
+    function create()
+    {
+        return view('todos.create');
     }
 
-    function store(Request $req){
-        
-        // $req->validate(['title' => 'required|max:255']);                                 //Default error message
-        $rules = ['title' => 'required|max:255'];                                           //Custom error message  
-        $messages = ['title.max' => 'Todo title should not exceeds 255 character !!!!!'] ;
-        $validator = Validator::make($req->all(), $rules, $messages);
-        if($validator->fails()){
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
+
+    //serving validation from Form Request which we made though [php artisan make:request TodoCreateRequest]=============================
+    function store(TodoCreateRequest $req)
+    {
+        // // $req->validate(['title' => 'required|max:255']);                                 //Default error message
+
+        // // $rules = ['title' => 'required|max:255'];                                        //Custom error message  
+        // $messages = ['title.max' => 'Todo title should not exceeds 255 character !!!!!'] ;
+        // $validator = Validator::make($req->all(), $rules, $messages);
+        // if($validator->fails()){
+        //     return redirect()->back()->withErrors($validator)->withInput();
+        // }
 
         Todo::create($req->all());
         return redirect()->back()->with('message', "You created something");
-
     }
 
-    function edit(){
-        
+
+
+    function edit(Todo $id)                         //type hinting model
+    {
+        return view('todos/edit', compact('id'));   //compact same as creating array and passing it to view
+    }
+
+
+    function update(Todo $id)                         
+    {
+        return view('todos/edit', compact('id'));   
     }
 }
-
