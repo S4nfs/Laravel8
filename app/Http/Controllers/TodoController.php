fae5ea8e486244dda7525c06a99c9c22f569bf9d
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-30 14:28:33 +0530

    oredred todo list by completed, added complete and incomplete

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -13,7 +13,7 @@ class TodoController extends Controller
 {
     function index()
     {
-        $mytodo = Todo::all();
+        $mytodo = Todo::orderby('completed')->get();            //ordered by completed 0,1
         return view ('todos.index', ['mytodos' => $mytodo]);
     }
 
@@ -38,18 +38,30 @@ class TodoController extends Controller
         Todo::create($req->all());
         return redirect()->back()->with('message', "You created something");
     }
-
-
+//======================================================================================================================================
 
     function edit(Todo $id)                         //type hinting model
     {
         return view('todos/edit', compact('id'));   //compact same as creating array and passing it to view
     }
+//======================================================================================================================================
 
-
-    function update(Todo $id, TodoCreateRequest $req)         //here we are passing both model and request(TodoCreateRequest) to keep code less
+    function update(Todo $id, TodoCreateRequest $req)//here we are passing both model and request(TodoCreateRequest) to keep code less
     {
         $id->update(['title'=> $req->title]);
         return redirect()->back()->with('message', "You updated something");
     }
+
+//======================================================================================================================================
+    function complete(Todo $id){
+        $id->update(['completed' => true]);
+        return redirect()->back()->with('message', "Hooray!! Task Completed");
+      
+    }
+
+    function incomplete(Todo $id){
+        $id->update(['completed' => false]);
+        return redirect()->back()->with('message', "Task Marked as Incompleted");
+    }
+
 }
