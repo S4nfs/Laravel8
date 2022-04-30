32e79949f4f4b965e958c38f6d053d103d2c88a7
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-30 14:28:53 +0530

    fillabled property added with completed

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable = ['title'];
+    protected $fillable = ['title', 'completed'];
 }
 
 
